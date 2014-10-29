578685d1f581fd4ccfb4025932b58427f8290ad8
Rup Shri <irhspur@gmail.com>
2014-10-29 21:26:02 +0545

    Profile edited

--- app/build/generated/source/r/debug/phonedirectory/lion/com/lion/R.java
+++ app/build/generated/source/r/debug/phonedirectory/lion/com/lion/R.java
@@ -931,6 +931,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020058;
         public static final int logo=0x7f020059;
         public static final int presidents_logo=0x7f02005a;
+        public static final int textview_back=0x7f02005b;
     }
     public static final class id {
         public static final int action_bar=0x7f080025;
@@ -944,21 +945,26 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f080015;
         public static final int action_menu_presenter=0x7f080016;
         public static final int action_mode_close_button=0x7f08002d;
-        public static final int action_search=0x7f08004e;
+        public static final int action_search=0x7f080056;
         public static final int activity_chooser_view_content=0x7f08002e;
-        public static final int address=0x7f080047;
+        public static final int address=0x7f08004b;
+        public static final int address_title=0x7f08004a;
         public static final int always=0x7f08000f;
         public static final int beginning=0x7f08000a;
-        public static final int blood=0x7f08004b;
+        public static final int blood=0x7f080053;
+        public static final int blood_title=0x7f080052;
         public static final int checkbox=0x7f080036;
-        public static final int club=0x7f08004c;
+        public static final int club=0x7f080054;
         public static final int collapseActionView=0x7f080011;
+        public static final int contact_header=0x7f080046;
         public static final int default_activity_button=0x7f080031;
         public static final int dialog=0x7f080012;
         public static final int disableHome=0x7f080008;
+        public static final int divider_contact=0x7f080047;
         public static final int dropdown=0x7f080013;
         public static final int edit_query=0x7f080039;
-        public static final int email=0x7f08004a;
+        public static final int email=0x7f080051;
+        public static final int email_title=0x7f080050;
         public static final int end=0x7f08000c;
         public static final int expand_activities_button=0x7f08002f;
         public static final int expanded_menu=0x7f080035;
@@ -970,15 +976,18 @@ containing a value of this type.
         public static final int listMode=0x7f080001;
         public static final int listVew=0x7f080018;
         public static final int list_item=0x7f080032;
-        public static final int location=0x7f08004d;
+        public static final int location=0x7f080055;
         public static final int middle=0x7f08000b;
-        public static final int mobile=0x7f080049;
-        public static final int name=0x7f080046;
+        public static final int mobile=0x7f08004f;
+        public static final int mobile_title=0x7f08004e;
+        public static final int name=0x7f080049;
+        public static final int name_title=0x7f080048;
         public static final int never=0x7f08000d;
         public static final int none=0x7f080009;
         public static final int normal=0x7f080000;
-        public static final int phone=0x7f080048;
-        public static final int president=0x7f080050;
+        public static final int phone=0x7f08004d;
+        public static final int phone_title=0x7f08004c;
+        public static final int president=0x7f080058;
         public static final int progress_circular=0x7f080019;
         public static final int progress_horizontal=0x7f08001a;
         public static final int radio=0x7f080038;
@@ -1000,7 +1009,7 @@ containing a value of this type.
         public static final int search_plate=0x7f08003f;
         public static final int search_src_text=0x7f080040;
         public static final int search_voice_btn=0x7f080044;
-        public static final int secratary=0x7f08004f;
+        public static final int secratary=0x7f080057;
         public static final int shortcut=0x7f080037;
         public static final int showCustom=0x7f080007;
         public static final int showHome=0x7f080004;
@@ -1014,7 +1023,7 @@ containing a value of this type.
         public static final int up=0x7f08002a;
         public static final int useLogo=0x7f080003;
         public static final int withText=0x7f080010;
-        public static final int z_chairman=0x7f080051;
+        public static final int z_chairman=0x7f080059;
     }
     public static final class integer {
         public static final int abc_max_action_buttons=0x7f0a0000;
@@ -1072,13 +1081,16 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0b000c;
         public static final int action_search=0x7f0b000d;
         public static final int action_settings=0x7f0b000e;
-        public static final int app_name=0x7f0b000f;
-        public static final int club_header=0x7f0b0010;
-        public static final int contact_header=0x7f0b0011;
-        public static final int hello_world=0x7f0b0012;
-        public static final int sample_info=0x7f0b0013;
-        public static final int title_activity_profile=0x7f0b0014;
-        public static final int welcome_text=0x7f0b0015;
+        public static final int address_title=0x7f0b000f;
+        public static final int app_name=0x7f0b0010;
+        public static final int club_header=0x7f0b0011;
+        public static final int contact_header=0x7f0b0012;
+        public static final int hello_world=0x7f0b0013;
+        public static final int name_profile=0x7f0b0014;
+        public static final int phone_title=0x7f0b0015;
+        public static final int sample_info=0x7f0b0016;
+        public static final int title_activity_profile=0x7f0b0017;
+        public static final int welcome_text=0x7f0b0018;
     }
     public static final class style {
         /**  Customize your theme here. 
